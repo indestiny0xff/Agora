e7a6414d8fd46c3d687680d89e063818123734e6
Othmane <othmanebelghazipro@gmail.com>
2024-06-19 10:03:58 +0200

    Update Agora.py

--- Agora.py
+++ Agora.py
@@ -49,7 +49,8 @@ news_feeds = {
     "Seqwrite": "https://www.seqrite.com/blog/tag/ransomware/feed/",
     "Hkcert": "https://www.hkcert.org/getrss/security-bulletin",
     "Enisa": "https://www.enisa.europa.eu/atom.xml",
-    "Securityboulevard": "https://securityboulevard.com/feed/"
+    "Securityboulevard": "https://securityboulevard.com/feed/",
+    "Privacyaffairs": "https://www.privacyaffairs.com/feed/"
 }   
 
 cve_feeds = {
